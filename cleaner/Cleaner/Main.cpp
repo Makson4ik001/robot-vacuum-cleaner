07031f0c856cd8187d975dcb25afbce6905877d2
nirvanaonearth <ivalera138@gmail.com>
2022-12-19 20:23:58 +0800

    managed to complete geometry.cpp

--- cleaner/Cleaner/Main.cpp
+++ cleaner/Cleaner/Main.cpp
@@ -16,14 +16,14 @@ int main() {
   SimpleGraphGenerator sgraphGenerator;
   SimplePathBuilder spathBuilder;
   SimpleGridGenerator sgridGenerator;
-  std::vector<Vector2> circuit = {Vector2(0, 0), Vector2(0, 10),
-                                  Vector2(-10, 10), Vector2(-10, 0),
-                                  Vector2(0, 0)};
+  std::vector<Vector2> circuit = {Vector2(0, 0), Vector2(2, -1), Vector2(1, 0),
+                                  Vector2(2, 2), Vector2(0, 0)};
   double radius = 10;
   // run(circuit, &sgridGenerator, &sgraphGenerator, &spathBuilder, &srenderer,
   // radius);
-  Vector2 inside(-2, 1), outside(100, 100);
-  cout << "alive" << endl;
-  cout << contains(circuit, inside) << " " << contains(circuit, outside);
+  Vector2 inside(0.5, 0), outside(1.2, 0.1);
+  int expectedTrue = contains(circuit, inside),
+      expectedFalse = contains(circuit, outside);
+  cout << expectedTrue << " " << expectedFalse;
   return 0;
 }
