78af30c4aa493ffa86a82dd7068e7d1c2bf32af3
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2023-01-25 18:17:45 +0500

    Implemented pathBuilder, graphGenerator

--- cleaner/Cleaner/Graph/SimplePathBuilder.cpp
+++ cleaner/Cleaner/Graph/SimplePathBuilder.cpp
@@ -1,8 +1,27 @@
 #include "SimplePathBuilder.h"
-
-std::vector<std::pair<std::size_t, double>>
-SimplePathBuilder::findPath(std::vector<std::vector<double>> const &graph) {
-  std::vector<std::pair<std::size_t, double>> result;
-  // TODO: find path
+#include "../Controller/GraphGenerator/SimpleGraphGenerator.h"
+#include <array>
+magic::Adjacency_list SimplePathBuilder::makeMagicGraph(std::vector<std::array<size_t, 4>> const &graph){
+  magic::Adjacency_list result(graph.size());
+  for(size_t i = 0; i < graph.size(); i++){
+    for(size_t j = 0; j < 4; j++){
+      if(graph[i][j] == NODE_NOT_STATED)
+        continue;
+      result[i].push_back({graph[i][j], 1});
+    }
+  }
   return result;
+}
+std::vector<Vector2> SimplePathBuilder::makeRegularPath(magic::Path path, std::vector<Vector2> points){
+  std::vector<Vector2> result;
+  for(auto node: path.indices){
+    result.push_back(points[node]);
+  }
+  return result;
+}
+std::vector<Vector2>
+  SimplePathBuilder::findPath(std::vector<std::array<size_t, 4>> const &graph, size_t node, std::vector<Vector2> points) {
+  auto mGraph = makeMagicGraph(graph);
+  auto mPath = magic::compute_minimal_full_path(mGraph, node, 10000);
+  return makeRegularPath(mPath, points);
 }
